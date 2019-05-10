7883d58a924a54810baa6f5c9f10c24aaa9be1e2
daniel120797 <d-silvana@hotmail.com>
2019-05-10 16:37:30 +0100

    create quiz class

--- Tests/EmployeeTest.java
+++ Tests/EmployeeTest.java
@@ -1,8 +1,12 @@
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeTest {
 
+    @Test
+    void getNameEmployee(){
 
+    }
 }
---
+++ src/People/Quiz.java
@@ -0,0 +1,63 @@
+package People;
+
+import java.util.HashMap;
+
+public class Quiz {
+    private String reason;
+    private String objective;
+    private String pathologies;
+    private String medication;
+    private String familyHistory;
+    private String phisicalActivity;
+    private String intestinalFunction;
+    private String waterConsuming;
+    private HashMap<String,String> chemistsAndBiologicalData;
+
+    public Quiz(String reason, String objective, String pathologies, String medication, String familyHistory, String phisicalActivity, String intestinalFunction, String waterConsuming, HashMap<String, String> chemistsAndBiologicalData) {
+        this.reason = reason;
+        this.objective = objective;
+        this.pathologies = pathologies;
+        this.medication = medication;
+        this.familyHistory = familyHistory;
+        this.phisicalActivity = phisicalActivity;
+        this.intestinalFunction = intestinalFunction;
+        this.waterConsuming = waterConsuming;
+        this.chemistsAndBiologicalData = chemistsAndBiologicalData;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public String getObjective() {
+        return objective;
+    }
+
+    public String getPathologies() {
+        return pathologies;
+    }
+
+    public String getMedication() {
+        return medication;
+    }
+
+    public String getFamilyHistory() {
+        return familyHistory;
+    }
+
+    public String getPhisicalActivity() {
+        return phisicalActivity;
+    }
+
+    public String getIntestinalFunction() {
+        return intestinalFunction;
+    }
+
+    public String getWaterConsuming() {
+        return waterConsuming;
+    }
+
+    public HashMap<String, String> getChemistsAndBiologicalData() {
+        return chemistsAndBiologicalData;
+    }
+}
