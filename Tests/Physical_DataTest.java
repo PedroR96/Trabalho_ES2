5c85554ff93cfedf052297cff9d6985fd9a8986d
miguel <miguelpvsm@hotmail.com>
2019-05-10 16:50:10 +0100

    Physical_data class and test

---
+++ Tests/Physical_DataTest.java
@@ -0,0 +1,14 @@
+import People.Person;
+import People.Physical_Data;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Physical_DataTest {
+    @Test
+    void checkPesoLimits(){
+        Physical_Data n1 = new Physical_Data(70,10,182,40,20,10,20,70,20,214);
+        Person P = new Person("Inacio","Apanhador de lixo",n1);
+
+    }
+}
